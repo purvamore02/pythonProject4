15776ade66f6a46f6888c79e42b14cd0c532d4fa
purvamore02 <143944909+purvamore02@users.noreply.github.com>
2024-03-07 12:18:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+email = st.text_input('Enter Email')
+password = st.text_input('Enter password')
+
+
+btn = st.button('Login')
+if btn:
+    if email == 'amangupta@gmail.com' and password == '12345':
+       st.balloons()
+    else:
+        st.error('Login Unsuccessful')
